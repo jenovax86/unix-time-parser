06f10a85e2f82dfc7435e8b034495a5857dc9d03
jenovax86 <shruzvn@gmail.com>
2023-12-30 02:40:41 +0330

    feat: add unix-time-stamp-parser

--- cmd/main.go
+++ cmd/main.go
@@ -1,7 +1,20 @@
 package main
 
+import (
+	"fmt"
+	"strconv"
+	"time"
+)
 
+func unixTimeStampParser(timeStamp string) {
+	numbers, err := strconv.ParseInt(timeStamp, 10, 32)
+	if err != nil {
+		panic("Invalid Timestamp")
+	}
+	readableTime := time.Unix(numbers, 0)
+	fmt.Println(readableTime)
+}
 
-func main(){
-	
+func main() {
+	unixTimeStampParser("1703890777")
 }
